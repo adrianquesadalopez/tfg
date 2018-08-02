ec492f958d4b1ebb8a23db7252b71b0de4aa6f0e
adquelo <adrianomanzano11@gmail.com>
2018-08-02 21:04:09 +0200

    Mostrar las opciones guardadas anteriormente

--- mymodcomments/mymodcomments.php
+++ mymodcomments/mymodcomments.php
@@ -63,5 +63,42 @@ class MyModComments extends Module
 
 		 }
 	}
+	public function assignProductTabComment() //para mostrar css i jvs en los comentarios
+	{
+	 $this->context->controller->addCSS($this->_path.'views/css/
+		 mymodcomments.css', 'all');
+		$this->context->controller->addJS($this->_path.'views/js/
+		 mymodcomments.js');
+		$this->context->smarty->assign('enable_grades', $enable_grades);
+		$this->context->smarty->assign('enable_comments',
+		 $enable_comments);
+		$this->context->smarty->assign('comments', $comments);
+	}
+
+
+	public function assignProductTabContent() //para mostrar los comentarios
+	{
+		$this->context->controller->addCSS($this->_path.'views/css/star-rating.css', 'all');
+	$this->context->controller->addJS($this->_path.'views/js/star-rating.js');
+	
+	 $enable_grades = Configuration::get('MYMOD_GRADES');
+	 $enable_comments = Configuration::get('MYMOD_COMMENTS');
+	 $id_product = Tools::getValue('id_product');
+	 $comments = Db::getInstance()->executeS('SELECT * FROM
+	 '._DB_PREFIX_.'mymod_comment WHERE id_product =
+	 '.(int)$id_product);
+	 $this->context->smarty->assign('enable_grades', $enable_grades);
+	 $this->context->smarty->assign('enable_comments',
+	 $enable_comments);
+	 $this->context->smarty->assign('comments', $comments);
+	 $this->processProductTabContent();
+	} 
+	
+	public function hookDisplayProductTabContent($params)//lanza el displayProductTabContent.tpl para que se muestre la posibilidad de poner reseñas en el front-end
+	{
+	 $this->processProductTabContent();
+	 $this->assignProductTabContent();
+	 return $this->display(__FILE__, 'displayProductTabContent.tpl');
+	}
 	 
 }
